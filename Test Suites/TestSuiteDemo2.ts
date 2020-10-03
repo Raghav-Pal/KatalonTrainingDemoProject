b3802d508c5950681b141f6ae09e9c4140fdc537
RaghavPal <raghav9901@gmail.com>
2020-10-03 02:51:07 +0000

    added TestSuiteDemo2

---
+++ Test Suites/TestSuiteDemo2.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteDemo2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>7</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>94c35791-9e45-491c-ace1-57ace595736c</testSuiteGuid>
+   <testCaseLink>
+      <guid>7dd544cc-db0e-4f1d-b9f3-a8dbf00fe3e5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/RecordModeDemo</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
